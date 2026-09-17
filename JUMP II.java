ae40f61c5c3b7daae5f3eda08cae8e9221060891
ANIRUDH <minatisahoo266@gmail.com>
2026-09-17 16:45:19 +0530

    Update JUMP II.java

--- JUMP II.java
+++ JUMP II.java
@@ -1,4 +1,11 @@
-class jumpII{
+//You are given a 0-indexed array of integers nums of length n. You are initially positioned at index 0.
+
+//Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at index i, you can jump to any index (i + j) where:
+
+//0 <= j <= nums[i] and
+//i + j < n
+//Return the minimum number of jumps to reach index n - 1. The test cases are generated such that you can reach index n - 1.
+    class jumpII{
     public int jump(int[]nums){
         int jumps=0;
         int currend=0;
